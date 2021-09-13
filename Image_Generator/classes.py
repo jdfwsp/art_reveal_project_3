0458b4bcaaccc26ec8edcdd3e625c179c607c7a2
jdfwsp <joshfergusonsp@gmail.com>
2021-09-13 08:37:51 -0500

    Creates Image class

--- Image_Generator/classes.py
+++ Image_Generator/classes.py
@@ -5,17 +5,21 @@ class Stats:
     
     def __init__(self, code):
         self.attack = statMap[code[0]]
-        self.defent = statMap[code[1]]
+        self.defend = statMap[code[1]]
         self.vitality = statMap[code[2]]
+        
+class Image:
+    
+    def __init__(self, code):
+        self.head = layers['Head'][code[0]]
+        self.body = layers['Body'][code[1]]
+        self.hat = layers['Hat'][code[2]]
         
 class Eagle:
     
     def __init__(self, code):
         self.code = code
         self.stats = Stats(code)
-        self.image = {
-            'Head': layers['Head'][code[0]],
-            'Body': layers['Body'][code[1]],
-            'Hat': layers['Hat'][code[2]]
-        }
+        self.image = Image(code)
+        
         
