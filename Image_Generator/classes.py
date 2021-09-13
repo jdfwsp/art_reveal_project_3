5609dcc453d328dea5f0284f6af1bc98b1b3d600
jdfwsp <joshfergusonsp@gmail.com>
2021-09-13 08:32:07 -0500

    Creates Stats class

--- Image_Generator/classes.py
+++ Image_Generator/classes.py
@@ -1,15 +1,18 @@
 from paths import layers
 from stats import statMap      
+   
+class Stats:
+    
+    def __init__(self, code):
+        self.attack = statMap[code[0]]
+        self.defent = statMap[code[1]]
+        self.vitality = statMap[code[2]]
         
 class Eagle:
     
     def __init__(self, code):
         self.code = code
-        self.stats = {
-            'Attack': statMap[code[0]],
-            'Defend': statMap[code[1]],
-            'Vitality': statMap[code[2]]
-        }
+        self.stats = Stats(code)
         self.image = {
             'Head': layers['Head'][code[0]],
             'Body': layers['Body'][code[1]],
