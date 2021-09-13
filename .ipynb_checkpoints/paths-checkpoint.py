08ed59ac2f772be9d1ba59cdd9546462cdc5d27a
jdfwsp <joshfergusonsp@gmail.com>
2021-09-13 07:59:26 -0500

    Creates Python working files

---
+++ .ipynb_checkpoints/paths-checkpoint.py
@@ -0,0 +1,17 @@
+layers = {'Head': {
+    'a': 'aaaa',
+    'b': 'bbbb',
+    'c': 'cccc'
+},
+    
+'Body': {
+    'a': 'aaaa',
+    'b': 'bbbb',
+    'c': 'cccc'
+},
+        
+'Hat': {
+    'a': 'aaaa',
+    'b': 'bbbb',
+    'c': 'cccc'
+}}
